8191071b5016e38f5266b88a52e22b9baf8a1941
Bartosz Ratajczyk <bartosz@Bartoszs-MacBook-Pro.local>
2024-09-19 21:31:15 +0200

    created CLI timer

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+func main() {
+	hours := flag.Int("h", 0, "Liczba godzin")
+	minutes := flag.Int("m", 0, "Liczba minut")
+	seconds := flag.Int("s", 0, "Liczba sekund")
+
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: timer -h=1 -m=30 -s=10")
+		return
+	}
+//3
+	s := spinner.New(spinner.CharSets[7], 1* time.Second)
+	s.Start()
+
+	totalSeconds := (*hours * 3600) + (*minutes * 60) + *seconds
+	for totalSeconds > 0  {
+		s.Start()
+		hs := totalSeconds / 3600
+		mins := (totalSeconds % 3600) / 60
+		sec := totalSeconds % 60
+
+		s.Suffix = fmt.Sprintf(" %02d:%02d:%02d", hs, mins, sec)
+
+		time.Sleep(1 * time.Second)
+		totalSeconds--
+	}
+	s.Stop()
+	playAlarm()
+}
+
+func playAlarm() {
+	cmd := exec.Command("afplay", "/System/Library/Sounds/Glass.aiff") // Zmień na wybrany dźwięk
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Błąd podczas odtwarzania alarmu:", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module taon/timer
+
+go 1.23.1
+
+require (
+	github.com/briandowns/spinner v1.23.1
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.24.0 // indirect
+)
